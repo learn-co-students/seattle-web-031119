e48627571a5f9866f7eb019e69b8a98fa779cb06
aspenjames <aspenjames@tqca.org>
2019-05-31 11:45:20 -0700

    lecture end

--- 45-intro-to-redux/index.js
+++ 45-intro-to-redux/index.js
@@ -11,18 +11,53 @@ const keypress = require('keypress');
  *
  */
 
+const reducer = (state, action) => {
+	console.log("action:", action)
+	switch (action.type) {
+		case "BUY_LUNCH": // => {type: "BUY_LUNCH", payload: 10}
+			return {balance: state.balance - action.payload}
+
+		case "BUY_CAR":
+			return {balance: state.balance - action.payload}
+		
+		case "GAMBLE":
+			let x = Math.random();
+			if (x < 0.5) {
+				return {balance: state.balance + action.payload}
+			} else {
+				return {balance: state.balance - action.payload}
+			}
+
+		case "GET_PAID":
+			return {balance: state.balance + action.payload}
+
+		case "DOSTUFF":
+			return {balance: state.balance + action.payload}
+			
+
+		default:
+			return state
+	}
+}
+
 const MENU = '(l) buy lunch (c) buy car (g) gamble (p) get paid (x) exit';
 
-// store.subscribe(() => {
-//   const state = store.getState(); // grab our state
-// 
-//   console.log();
-//   console.log('Your bank account: ', state); // print out our balance
-// 
-//   console.log();
-//   console.log(MENU); // display the menu
-//   console.log();
-// });
+const initialState = { balance: 75 }
+
+const store = redux.createStore(reducer, initialState)
+
+store.subscribe(() => {
+  const state = store.getState(); // grab our state
+
+  console.log();
+  console.log('Your bank account: ', state); // print out our balance
+
+  console.log();
+  console.log(MENU); // display the menu
+  console.log();
+});
+
+store.dispatch({type: "RANDOM_ACTION"})
 
 // This next bit sets up our CLI
 keypress(process.stdin)
@@ -30,19 +65,25 @@ keypress(process.stdin)
 process.stdin.on('keypress', (_ch, key) => {
   if (key) switch (key.name) {
     case 'l':
-      store.dispatch({type: /* buy lunch */});
+      store.dispatch({type: "DOSTUFF", payload: -10});
       break;
 
     case 'c':
-      store.dispatch({type: /* buy car */});
+      store.dispatch({type: "DOSTUFF", payload: -75000});
       break;
 
     case 'g':
-      store.dispatch({type: /* gamble  */});
-      break;
+			let x = Math.random()
+			if (x < 0.5) {
+				store.dispatch({type: "DOSTUFF", payload: -75000});
+				break;
+			} else {
+				store.dispatch({type: "DOSTUFF", payload: 250000});
+				break;
+			}
 
     case 'p':
-      store.dispatch({type: /* get paid */});
+      store.dispatch({type: "DOSTUFF", payload: 8});
       break;
 
     case 'x':
